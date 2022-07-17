720feb0897fb8c94694793cc6922205d23127a9f
Giancarlo Oblitas <giancarlooblitas97@gmail.com>
2022-07-16 21:28:55 -0300

    primer version

---
+++ complementario.js
@@ -0,0 +1,30 @@
+// item 1
+
+let ingresarNumero = parseInt(prompt("Ingresar Numero")); 
+
+for (let i = 1; i <= 10; i++) {
+    let resultado = ingresarNumero + i ;
+    console.log( ingresarNumero + " + " , i , " = " , resultado)
+
+    alert(ingresarNumero +" + "+ i +" = "+ resultado);
+}
+
+//item2
+/*
+let entrada = prompt("Ingresar un dato");
+
+while(entrada != "ESC" ){
+    alert("El usuario ingresó "+ entrada);
+    
+    entrada = prompt("Ingresar otro dato");
+}
+*/
+//item3 
+/*
+let ingresarNumero = parseInt(prompt("Ingresar Numero")); 
+
+for (let i = 1; i <= ingresarNumero; i++) {
+
+    alert(i + " HOLA " )
+}
+*/
